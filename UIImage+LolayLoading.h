3c47082a62e46994724b520c86d0c5b976c96e21
Gary Rudolph <GaryRudolph@me.com>
2011-01-15 22:27:57 -0800

    Imported

---
+++ UIImage+LolayLoading.h
@@ -0,0 +1,13 @@
+//
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
+//
+// This is based on the code found in a Stack Overflow post
+// http://stackoverflow.com/questions/1815476/cgimage-uiimage-lazily-loading-on-ui-thread-causes-stutter
+#import <Foundation/Foundation.h>
+
+@interface UIImage (LolayLoading)
+
+- (void) forceLoad;
+
+@end
---
+++ UIImage+LolayLoading.m
@@ -0,0 +1,26 @@
+//
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
+//
+#import "UIImage+LolayLoading.h"
+
+@implementation UIImage (LolayLoading)
+
+- (void) forceLoad {
+	const CGImageRef cgImage = [self CGImage];  
+
+	const int width = CGImageGetWidth(cgImage);
+	const int height = CGImageGetHeight(cgImage);
+
+	const CGColorSpaceRef colorspace = CGImageGetColorSpace(cgImage);
+	const CGContextRef context = CGBitmapContextCreate(
+													   NULL, /* Where to store the data. NULL = don’t care */
+													   width, height, /* width & height */
+													   8, width * 4, /* bits per component, bytes per row */
+													   colorspace, kCGImageAlphaNoneSkipFirst);
+	
+	CGContextDrawImage(context, CGRectMake(0, 0, width, height), cgImage);
+	CGContextRelease(context);
+}
+
+@end
